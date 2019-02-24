bee37ea3b9e332c94e302bf635705a2bf251a758
DevMai90 <dmai90@yahoo.com>
2019-02-23 23:52:25 -0800

    Added contactReducer and brought in Provider

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,9 @@
+// Each reducer has its own initial state
+const initialState = {};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+// Meeting place for all of our reducers
+import { combineReducers } from 'redux';
+import contactReducer from './contactReducer';
+
+// Export all of our reducers as an object
+export default combineReducers({
+  contact: contactReducer
+});
